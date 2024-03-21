a6654945a98d20e332fda257cd05467442ca408c
Perry Xie <perryxie.nz@gmail.com>
2024-03-21 19:31:55 +1300

    Updated the venue array system to be a part of the VenueHireSystem class rather than the Venue class. Created a check for exisitng venue codes when creating a new venue.

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -1,13 +1,12 @@
 package nz.ac.auckland.se281;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Venue {
   private String name;
   private String code;
   private String cap;
   private String fee;
-  ArrayList<Venue> venueList = new ArrayList<Venue>();
 
   public Venue(String venueName, String venueCode, String capacityInput, String hireFeeInput){
     name = venueName;
@@ -16,29 +15,20 @@ public class Venue {
     fee = hireFeeInput;
   }
 
-  public void addVenue(Venue venue){
-    venueList.add(venue);
-  }
-  public void addVenueSuccess(Venue venue){
-    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(name, code);
+  public String getName(){
+    return name;
   }
 
-  public void addVenueFail(Venue venue){
-
-  }
-  public boolean venueValid(Venue venue){
-    return venue.checkName(name);
+  public String getCode(){
+    return code;
   }
 
-  public boolean checkName(String name){
-    String cleanName = name.replaceAll("\\s+","");
-    int nameLength = cleanName.length();
-    if (nameLength == 0){
-      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
-      return false;
-    }
-    else{
-      return true;
-    }
+  public String getCap(){
+    return cap;
   }
+
+  public String getFee(){
+    return fee;
+  }
+
 }
--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -6,8 +6,12 @@ import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
+  //private ArrayList<Venue> venueList;
+  ArrayList<Venue> venueList = new ArrayList<Venue>();
 
-  public VenueHireSystem() {}
+  public VenueHireSystem() {
+    //ArrayList<Venue> venueList = new ArrayList<Venue>();
+  }
 
   public void printVenues() {
     // TODO implement this method
@@ -16,11 +20,11 @@ public class VenueHireSystem {
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
         Venue venue = new Venue(venueName, venueCode, capacityInput, hireFeeInput);
-        if (venue.venueValid(venue) == true){
-          venue.addVenue(venue);
-          venue.addVenueSuccess(venue);}
+        if (venueValid(venue) == true){
+          addVenue(venue);
+          addVenueSuccess(venue);}
         else{
-          venue.addVenueFail(venue);
+          addVenueFail(venue);
         }
   }
 
@@ -55,4 +59,58 @@ public class VenueHireSystem {
   public void viewInvoice(String bookingReference) {
     // TODO implement this method
   }
+
+  //Adds Venue to Arraylist
+  public void addVenue(Venue venue){
+    venueList.add(venue);
+  }
+
+  //Venue success message
+  public void addVenueSuccess(Venue venue){
+    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venue.getName(), venue.getCode());
+  }
+
+  public void addVenueFail(Venue venue){
+
+  }
+
+  //Checks if venue is valid
+  public boolean venueValid(Venue venue){
+    if (checkName(venue.getName()) == false){
+      return false;
+    }
+    else if (checkCode(venue.getCode()) == false){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
+  //Checks if name is valid
+  public boolean checkName(String nameString){
+    String cleanName = nameString.replaceAll("\\s+","");
+    int nameLength = cleanName.length();
+    if (nameLength == 0){
+      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
+  //Checks if code is valid
+  public boolean checkCode(String codeString){
+
+    for (Venue venue : venueList) {
+      if (codeString.equals(venue.getCode()) == true){
+        MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(codeString, venue.getName());
+        return false;
+      }
+    }
+    return true;
+  }
+
+
 }
